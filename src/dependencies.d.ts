7c69bb55bc7f223f989a2de4a6710537459da1d2
DuroCodes <davidwright13503@gmail.com>
2024-06-26 16:24:26 -0400

    fix: remove dotenv dependency

--- src/dependencies.d.ts
+++ src/dependencies.d.ts
@@ -4,18 +4,22 @@
  * Service(s) api rely on this file to provide a better developer experience.
  */
 
-import type { Logging, ErrorHandling, CoreDependencies, Singleton } from '@sern/handler'
-import type { Client } from 'discord.js'
+import type {
+  Logging,
+  ErrorHandling,
+  CoreDependencies,
+  Singleton,
+} from "@sern/handler";
+import type { Client } from "discord.js";
 
 /**
  * Note: You usually would not need to modify this unless there is an urgent need to break the contracts provided.
  * You would need to modify this to add your custom Services, however.
  */
 declare global {
-   interface Dependencies extends CoreDependencies {
-        '@sern/client': Singleton<Client>
-   }
+  interface Dependencies extends CoreDependencies {
+    "@sern/client": Singleton<Client>;
+  }
 }
 
-
-export {}
+export {};
